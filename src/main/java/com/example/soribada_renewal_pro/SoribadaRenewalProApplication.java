10ed5f6255c54da0860df73640b0f7c87d506949
woohyeonju <woohyeonju507@gmail.com>
2023-02-23 15:12:44 +0900

    up

---
+++ src/main/java/com/example/soribada_renewal_pro/SoribadaRenewalProApplication.java
@@ -0,0 +1,13 @@
+package com.example.soribada_renewal_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoribadaRenewalProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoribadaRenewalProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/soribada_renewal_pro/SoribadaRenewalProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soribada_renewal_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoribadaRenewalProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
